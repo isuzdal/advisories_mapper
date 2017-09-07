59937ca6e9aab136e5df027502aa8b477adb218a
Ivan Suzdal <isuzdal@mirantis.com>
2017-09-07 14:59:08 +0300

    Change timeout in fetch_content

--- lib/utils.py
+++ lib/utils.py
@@ -5,7 +5,7 @@ except ImportError:
     # Python 3
     import urllib.request as urllib2
 
-def fetch_content(uri,tmout=10):
+def fetch_content(uri,tmout=30):
     try:
         body = urllib2.urlopen(uri,timeout=tmout).read()
     except:
